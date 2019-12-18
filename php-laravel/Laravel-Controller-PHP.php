700a959b67b368b7f5ae259cd9e6940e46353875
ggportfoliowork <39774697+ggportfoliowork@users.noreply.github.com>
2019-12-18 07:48:07 -0500

    Update Laravel-Controller-PHP.php

--- php-laravel/Laravel-Controller-PHP.php
+++ php-laravel/Laravel-Controller-PHP.php
@@ -1,158 +1,41 @@
 <?php
 
-namespace App\Http\Controllers\CohortPatient;
+namespace App\Http\Controllers\Api\Media;
 
-use DB;
-use App\Models\Cycle;
-use App\Models\Cohort;
-use App\Models\Patient;
-use Illuminate\Http\Request;
+use Response;
+use App\Models\Media;
 use App\Http\Controllers\Controller;
-use App\Transformers\CohortTransformer;
-use App\Services\Attendance\AttendanceCreator;
+use App\Services\Media\MediaCreator;
+use App\Services\Media\MediaUpdater;
+use App\Services\Media\MediaProcessor;
+use App\Http\Resources\Media\MediaResource;
+use App\Http\Requests\Media\CreateMediaRequest;
+use App\Http\Requests\Media\UpdateMediaRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
-/**
- * @resource (api) Cohort Membership
- *
- * @package App\Http\Controllers\CohortPatient
- */
-class CohortPatientController extends Controller
+class MediaController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     */
-    public function __construct()
+    public function show(Media $media)
     {
-        //
+        return new MediaResource($media);
     }
 
-
-    /**
-     * Add Patient to Cohort
-     *
-     * The user adding the patient and the patient must be a member of the site to which the Cohort belongs.
-     * The patient must also NOT be in another active cohort. A patient may belong to only 1 active cohort at a time.
-     *
-     * @param Cohort            $cohort
-     * @param Patient           $patient
-     * @param CohortTransformer $transformer
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Cohort $cohort, Patient $patient, CohortTransformer $transformer, AttendanceCreator $attendanceCreator)
+    public function store(CreateMediaRequest $request, MediaProcessor $mediaProcessor, MediaCreator $creator)
     {
-        # TODO : Move all of this logic out of the controller.
-        $this->setMeAndNow();
-        if ($this->me->cannot('addPatient', [$cohort, $patient])) {
-            return abort(403, 'You cannot add this patient to this cohort. They may already be in an active cohort. A patient can be in only 1 active cohort.');
-        }
-
-        # Start a new cycle (they must have a good eligibility)
-        $eligibility = $patient->open_eligibilities()->latest()->first();
-
-        if (empty($eligibility)) {
-            return abort(403, 'This patient does not have the required eligibility to begin an education cycle.');
-        }
-
-        $next_cycle_num = Cycle::getNextCycleNumForPatient($patient);
-
-        $cycle = Cycle::updateOrCreate([
-            'patient_id' => $patient->id,
-            'eligibility_id' => $eligibility->id,
-            'cycle_num' => $next_cycle_num,
-        ],[
-            'patient_id' => $patient->id,
-            'eligibility_id' => $eligibility->id,
-            'cycle_num' => $next_cycle_num,
-            'status' => ''
-        ]);
-
-        $cohort->patients()->attach($patient->id, [
-            'cycle_id' => $cycle->id,
-            'created_by' => $this->me->id,
-            'created_at' => $this->now
-        ]);
-
-        # Add blank attendance for this patient for all sessions of this cohort
-        $cohort->sessions->each(function($session) use ($attendanceCreator, $patient) {
-            $attendanceCreator->create($session, [$patient]);
-        });
-
-        $cohort->load(['patients' => function($q) {
-            $q->select('patients.id', 'name_first', 'name_middle', 'name_last', 'patients.patient_id', 'dob')
-                ->orderBy('patients.name_last', 'ASC');
-        }]);
-
-        return $this->responder()->respond([
-            'success' => true,
-            'message' => 'Patient added.',
-            'data' => [
-                'cohort' => $transformer->transformDetail($cohort)
-            ]
-        ]);
+        $file = $mediaProcessor->moveUploadedFileToTemporaryStorage($request->file);
+        $media = $creator->create($request->except('file'), $file);
+        return new MediaResource($media);
     }
 
-
-    /**
-     * Remove Patient from Cohort
-     *
-     * The user removing the patient and the patient must be a member of the site to which the Cohort belongs.
-     *
-     * @param Cohort            $cohort
-     * @param Patient           $patient
-     * @param CohortTransformer $transformer
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Cohort $cohort, Patient $patient, CohortTransformer $transformer)
+    public function update(Media $media, UpdateMediaRequest $request, MediaUpdater $updater)
     {
-        $this->setMeAndNow();
-        if ($this->me->cannot('removePatient', [$cohort, $patient])) {
-            return abort(403, 'You cannot remove this patient from this cohort.');
-        }
-
-        DB::table('cohort_patient')
-            ->where('patient_id', $patient->id)
-            ->where('cohort_id', $cohort->id)
-            ->whereNull('deleted_at')
-            ->update([
-                'deleted_at' => $this->now,
-                'deleted_by' => $this->me->id
-            ]);
-
-        $cohort->load(['patients' => function($q) {
-            $q->select('patients.id', 'name_first', 'name_middle', 'name_last', 'patients.patient_id', 'dob')
-                ->orderBy('patients.name_last', 'ASC');
-        }]);
-
-        return $this->responder()->respond([
-            'success' => true,
-            'message' => 'Patient removed.',
-            'data' => [
-                'cohort' => $transformer->transformDetail($cohort)
-            ]
-        ]);
+        $media = $updater->update($media, $request->all());
+        return new MediaResource($media);
     }
 
-
-    /**
-     * List Cohorts
-     *
-     * Lists the cohorts at a specified site.
-     * This endpoint is pagination-enabled.
-     *
-     * @param Site               $site The site to show the cohort list for
-     * @param Request            $request
-     * @param CohortPaginator    $paginator
-     * @param CohortTransformer  $transformer
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Site $site, Request $request, CohortPaginator $paginator, CohortTransformer $transformer)
+    public function destroy(Media $media)
     {
-        if ($this->user->cannot('view', $site)) {
-            return abort(403, 'You cannot view cohorts at this site.');
-        }
-        $paginated = $paginator->paginate($request);
-        $transformer->transformCollection($paginated->getCollection(), 'listing');
-        return $this->responder()->respond($paginated);
+        $media->delete();
+        return $this->responder()->json(['message' => "The media file has been deleted."]);
     }
-
 }
